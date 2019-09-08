a5bb4e36c9eb2e75358b601e576a8df69aeea37c
KimsoftStudio <55048494+KimsoftStudio@users.noreply.github.com>
2019-09-08 11:54:02 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'NjIwMTYwMTk0MTIyNDgxNjg1.XXTSgw.tUaj1UorC5yf1VYI8GwHC7ipe_o';
+const token = BOT_TOKEN;
 
 bot.on('ready', () =>{
     console.log('This bot is online!');
