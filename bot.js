01953e8ebfa6c8a962fda3fbee742c63eb89ffc6
KimsoftStudio <55048494+KimsoftStudio@users.noreply.github.com>
2019-09-08 11:49:07 -0600

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NjIwMTYwMTk0MTIyNDgxNjg1.XXTSgw.tUaj1UorC5yf1VYI8GwHC7ipe_o';
+
+bot.on('ready', () =>{
+    console.log('This bot is online!');
+})
+
+bot.on('message', msg => {
+    if(msg.content.includes('69')) {
+        msg.channel.sendMessage('Nice');
+    }
+    if(msg.content.includes('kinky')) {
+        msg.channel.sendMessage('( ͡° ͜ʖ ͡°)');
+    }
+    if(msg.content.includes('Kinky')) {
+        msg.channel.sendMessage('( ͡° ͜ʖ ͡°)');
+    }
+    if(msg.content.includes('uwu')) {
+        msg.channel.sendMessage('Degenerate');
+    }
+    if(msg.content.includes('owo')) {
+        msg.channel.sendMessage('Degenerate');
+    }
+    if(msg.content.includes('OWO')) {
+        msg.channel.sendMessage('Degenerate');
+    }
+    if(msg.content.includes('UWU')) {
+        msg.channel.sendMessage('Degenerate');
+    }
+    if(msg.content.includes('OwO')) {
+        msg.channel.sendMessage('Degenerate');
+    }
+    if(msg.content.includes('UwU')) {
+        msg.channel.sendMessage('Degenerate');
+    }
+    if(msg.content.includes('Hotel?')) {
+        msg.channel.sendMessage('Trivago');
+    }
+    if(msg.content.includes('Hotel')) {
+        msg.channel.sendMessage('Trivago');
+    }
+    if(msg.content.includes('Creeper')) {
+        msg.channel.sendMessage('AWW MAN');
+    }
+    if(msg.content.includes('creeper')) {
+        msg.channel.sendMessage('AWW MAN');
+    }
+    if(msg.content.includes('420')) {
+        msg.channel.sendMessage('Blaze It');
+    }
+    
+})
+
+bot.login(token);
