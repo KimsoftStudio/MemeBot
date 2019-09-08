9b6d5210a44415e869cf8d386eeef8770429114d
KimsoftStudio <55048494+KimsoftStudio@users.noreply.github.com>
2019-09-08 14:42:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,25 +16,22 @@ bot.on('message', message => {
         message.channel.sendMessage('( ͡° ͜ʖ ͡°)');
     }
     if(message.content.includes('uwu')) {
-        message.channel.sendMessage('Degenerate');
+        message.channel.sendMessage('You Degenerate');
     }
     if(message.content.includes('owo')) {
-        message.channel.sendMessage('Degenerate');
+        message.channel.sendMessage('You Degenerate');
     }
     if(message.content.includes('OWO')) {
-        message.channel.sendMessage('Degenerate');
+        message.channel.sendMessage('You Degenerate');
     }
     if(message.content.includes('UWU')) {
-        message.channel.sendMessage('Degenerate');
+        message.channel.sendMessage('You Degenerate');
     }
     if(message.content.includes('OwO')) {
-        message.channel.sendMessage('Degenerate');
+        message.channel.sendMessage('You Degenerate');
     }
     if(message.content.includes('UwU')) {
-        message.channel.sendMessage('Degenerate');
-    }
-    if(message.content.includes('Hotel?')) {
-        message.channel.sendMessage('Trivago');
+        message.channel.sendMessage('You Degenerate');
     }
     if(message.content.includes('Hotel')) {
         message.channel.sendMessage('Trivago');
@@ -47,6 +44,42 @@ bot.on('message', message => {
     }
     if(message.content.includes('420')) {
         message.channel.sendMessage('Blaze It');
+    }
+    if(message.content.includes('6ix9ine')) {
+        message.channel.sendMessage('Oof. Wrong one, bud.');
+    }
+    if(message.content.includes('6ix 9ine')) {
+        message.channel.sendMessage('Oof. Wrong one, bud.');
+    }
+    if(message.content.includes('six nine')) {
+        message.channel.sendMessage('Nice');
+    }
+    if(message.content.includes('sixty nine')) {
+        message.channel.sendMessage('Nice');
+    }
+    if(message.content.includes('sixty-nine')) {
+        message.channel.sendMessage('Nice');
+    }
+    if(message.content.includes('sixtynine')) {
+        message.channel.sendMessage('Nice');
+    }
+    if(message.content.includes('sixnine')) {
+        message.channel.sendMessage('Nice');
+    }
+    if(message.content.includes('watch this')) {
+        message.channel.sendMessage('┬┴┬┴┤(･_├┬┴┬┴');
+    }
+    if(message.content.includes('Watch this')) {
+        message.channel.sendMessage('┬┴┬┴┤(･_├┬┴┬┴');
+    }
+    if(message.content.includes('watch')) {
+        message.channel.sendMessage('┬┴┬┴┤(･_├┬┴┬┴');
+    }
+    if(message.content.includes('Watch')) {
+        message.channel.sendMessage('┬┴┬┴┤(･_├┬┴┬┴');
+    }
+    if(message.isMentioned('69Bot')) {
+        message.channel.sendMessage('Ay, das me');
     }
     
 });
