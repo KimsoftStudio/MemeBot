8c8a4e69691e4458010b512a9b08d4c5f4602623
KimsoftStudio <55048494+KimsoftStudio@users.noreply.github.com>
2020-04-16 20:58:29 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,6 +117,9 @@ bot.on('message', message => {
     if(message.content.includes("69Bot buy a new one")) {
         message.channel.sendMessage('!', {files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/discordIcon.png"]});
     }
+    if(message.content.includes("nani")) {
+        message.channel.sendMessage({files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/Nani_P_Lilo_and_Stitch.jpg"]});
+    }
 });
 
 bot.login(process.env.BOT_TOKEN);
