9c620f8e0b4495580b089b0cfb4487281b5a3ba2
KimsoftStudio <55048494+KimsoftStudio@users.noreply.github.com>
2021-05-10 00:39:09 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const bot = new Discord.Client();
 
 bot.on('ready', () =>{
     console.log('This bot is online!');
+    client.user.setActivity('Beans')
+    console.clear()
 });
 
 bot.on('message', message => {
@@ -141,22 +143,6 @@ bot.on('message', message => {
     if(message.content.includes("Flacid")) {
         message.channel.sendMessage({files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/Flacid_Snnake.png"]});
     }
-    static void UpdatePresence()
-{
-    DiscordRichPresence discordPresence;
-    memset(&discordPresence, 0, sizeof(discordPresence));
-    discordPresence.state = "Playing Solo";
-    discordPresence.details = "Competitive";
-    discordPresence.startTimestamp = 1507665886;
-    discordPresence.endTimestamp = 1507665886;
-    discordPresence.largeImageText = "Numbani";
-    discordPresence.smallImageText = "Rogue - Level 100";
-    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
-    discordPresence.partySize = 1;
-    discordPresence.partyMax = 5;
-    discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
-    Discord_UpdatePresence(&discordPresence);
-}
 });
 
 bot.login(process.env.BOT_TOKEN);
