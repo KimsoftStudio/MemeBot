3902c4db27f7a836dee606d3330dd57f23589418
KimsoftStudio <55048494+KimsoftStudio@users.noreply.github.com>
2021-05-17 00:46:43 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,9 @@ bot.on('ready', () =>{
     bot.user.setStatus('available')
     bot.user.setPresence({
         game: {
-            name: 'funny memes',
+            name: '?',
             type: "STREAMING",
-            url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            url: "https://www.youtube.com/watch?v=YwmqgG5ABH4"
         }
      });
 });
