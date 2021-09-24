f7539c4ec987f82832ccdffe925a48a6ea02bccd
KimsoftStudio <55048494+KimsoftStudio@users.noreply.github.com>
2021-09-24 00:51:50 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,119 +8,119 @@ bot.on('ready', () =>{
         game: {
             name: '!plshelp',
             type: "PLAYING",
-            url: 'https://www.youtube.com/watch?v=M75VLQuFPrY'
+            url: 'https://www.youtube.com/watch?v=fC7oUOUEEi4'
         }
      });
 });
 
 bot.on('message', message => {
     if(message.content.includes('69')) {
-        message.channel.sendMessage('dj12eUR2cHdwUlBNNA==');
+        message.channel.sendMessage('Nice');
     }
     if(message.content.includes('uwu')) {
-        message.channel.sendMessage('v=qWXnt2Z2D1E');
+        message.channel.sendMessage('You degenerate.');
     }
     if(message.content.includes('owo')) {
-        message.channel.sendMessage('v=qWXnt2Z2D1E');
+        message.channel.sendMessage('You degenerate');
     }
     if(message.content.includes('UwU')) {
-        message.channel.sendMessage('v=qWXnt2Z2D1E');
+        message.channel.sendMessage('You degenerate');
     }
     if(message.content.includes('OwO')) {
-        message.channel.sendMessage('v=qWXnt2Z2D1E');
+        message.channel.sendMessage('You degenerate');
     }
     if(message.content.includes('UWU')) {
-        message.channel.sendMessage('v=qWXnt2Z2D1E');
+        message.channel.sendMessage('You degenerate');
     }
     if(message.content.includes('OWO')) {
-        message.channel.sendMessage('v=qWXnt2Z2D1E');
+        message.channel.sendMessage('You degenerate');
     }
     if(message.content.includes('kinky')) {
-        message.channel.sendMessage('The clock is ticking');
+        message.channel.sendMessage('( ͡° ͜ʖ ͡°)');
     }
     if(message.content.includes('sexy')) {
-        message.channel.sendMessage('The clock is ticking');
+        message.channel.sendMessage('( ͡° ͜ʖ ͡°)');
     }
     if(message.content.includes('Sexy')) {
-        message.channel.sendMessage('The clock is ticking');
+        message.channel.sendMessage('( ͡° ͜ʖ ͡°)');
     }
     if(message.content.includes('Kinky')) {
-        message.channel.sendMessage('The clock is ticking');
+        message.channel.sendMessage('( ͡° ͜ʖ ͡°)');
     }
     if(message.content.includes('hotel')) {
-        message.channel.sendMessage('Do you see it?');
+        message.channel.sendMessage('Trivago');
     }
     if(message.content.includes('Hotel')) {
-        message.channel.sendMessage('Do you see it?');
+        message.channel.sendMessage('Trivago');
     }
     if(message.content.includes('creeper')) {
-        message.channel.sendMessage('Do you see it?');
+        message.channel.sendMessage('AWW MAN');
     }
     if(message.content.includes('Creeper')) {
-        message.channel.sendMessage('Do you see it?');
+        message.channel.sendMessage('AWW MAN');
     }
     if(message.content.includes('6ix9ine')) {
-        message.channel.sendMessage('v=xADSDapqn9o');
+        message.channel.sendMessage('Oof. Wrong one bud.');
     }
     if(message.content.includes('6ix 9ine')) {
-        message.channel.sendMessage('v=xADSDapqn9o');
+        message.channel.sendMessage('Oof. Wrong one bud.');
     }
     if(message.content.includes('Watch')) {
-        message.channel.sendMessage('https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/happyvalley.png');
+        message.channel.sendMessage('┬┴┬┴┤(･_├┬┴┬┴');
     }
     if(message.content.includes('watch')) {
-        message.channel.sendMessage('https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/happyvalley.png');
-    }
-    if(message.content.includes('youtu.be')) {
-        message.channel.sendMessage('https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/happyvalley.png');
+        message.channel.sendMessage('┬┴┬┴┤(･_├┬┴┬┴');
     }
     if(message.content.includes('six nine')) {
-        message.channel.sendMessage('v=M75VLQuFPrY');
+        message.channel.sendMessage('Nice');
     }
     if(message.content.includes('sixnine')) {
-        message.channel.sendMessage('v=M75VLQuFPrY');
+        message.channel.sendMessage('Nice');
     }
     if(message.content.includes('sixty nine')) {
-        message.channel.sendMessage('v=M75VLQuFPrY');
+        message.channel.sendMessage('Nice');
     }
     if(message.content.includes('egg')) {
-        message.channel.sendMessage('v=M75VLQuFPrY');
+        message.channel.sendMessage('I see you are cultured as well.');
     }
     if(message.content.includes('Egg')) {
-        message.channel.sendMessage('v=M75VLQuFPrY');
+        message.channel.sendMessage('I see you are cultured as well.');
     }
     if(message.content.includes('EgG')) {
-        message.channel.sendMessage('v=M75VLQuFPrY');
+        message.channel.sendMessage('I see you are cultured as well.');
     }
     if(message.content.includes('eGg')) {
-        message.channel.sendMessage('v=M75VLQuFPrY');
+        message.channel.sendMessage('I see you are cultured as well.');
     }
     if(message.content.includes('egG')) {
-        message.channel.sendMessage('v=M75VLQuFPrY');
+        message.channel.sendMessage('I see you are cultured as well.');
     }
     if(message.content.includes('EGG')) {
-        message.channel.sendMessage('v=M75VLQuFPrY');
+        message.channel.sendMessage('I see you are cultured as well.');
     }
     if(message.content.includes('sixtynine')) {
-        message.channel.sendMessage('v=xADSDapqn9o');
+        message.channel.sendMessage('Nice');
     }
     if(message.content.includes('sixty-nine')) {
-        message.channel.sendMessage('v=xADSDapqn9o');
+        message.channel.sendMessage('Nice');
     }
     if(message.content.includes("you're dead")) {
-        message.channel.sendMessage('lost.... cold....');
+        message.channel.sendMessage('F');
     }
     if(message.content.includes("( ͡° ͜ʖ ͡°)")) {
-        message.channel.sendMessage('A beginning to an end...');
+        message.channel.sendMessage('Deli', {files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/deli.png"]});
     }
     if(message.content.includes("jpeg")) {
-        message.channel.sendMessage('A beginning to an end...');
+        message.channel.sendMessage('I Just want of picture of a gotdang hotdog', {files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/jpeg.gif"]});
+    }
+    if(message.content.includes(".jpg")) {
+        message.channel.sendMessage('I Just want of picture of a gotdang hotdog', {files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/jpeg.gif"]});
     }
     if(message.content.includes("Jpeg")) {
-        message.channel.sendMessage('A beginning to an end...');
+        message.channel.sendMessage('I Just want of picture of a gotdang hotdog', {files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/jpeg.gif"]});
     }
     if(message.content.includes("Jpg")) {
-        message.channel.sendMessage('A beginning to an end...');
+        message.channel.sendMessage('I Just want of picture of a gotdang hotdog', {files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/jpeg.gif"]});
     }
     if(message.content.includes("69bot buy a new one")) {
         message.channel.sendMessage('!', {files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/discordIcon.png"]});
@@ -135,89 +135,86 @@ bot.on('message', message => {
         message.channel.sendMessage('!', {files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/discordIcon.png"]});
     }
     if(message.content.includes("nani")) {
-        message.channel.sendMessage('https://unknown-developer.itch.io/dont-play-this-game');
+        message.channel.sendMessage({files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/naniN.jpg"]});
     }
     if(message.content.includes("Nani")) {
-        message.channel.sendMessage('https://unknown-developer.itch.io/dont-play-this-game');
+        message.channel.sendMessage({files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/naniN.jpg"]});
     }
     if(message.content.includes("snake")) {
-        message.channel.sendMessage('v=Wmhfn3mgWUI');
+        message.channel.sendMessage({files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/Flacid_Snnake.png"]});
     }
     if(message.content.includes("flacid")) {
-        message.channel.sendMessage('v=YwmqgG5ABH4');
+        message.channel.sendMessage({files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/Flacid_Snnake.png"]});
     }
     if(message.content.includes("Snake")) {
-        message.channel.sendMessage('v=Wmhfn3mgWUI');
+        message.channel.sendMessage({files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/Flacid_Snnake.png"]});
     }
     if(message.content.includes("Flacid")) {
-        message.channel.sendMessage('v=YwmqgG5ABH4');
+        message.channel.sendMessage({files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/Flacid_Snnake.png"]});
     }
     if(message.content.includes("wah")) {
-        message.channel.sendMessage('lost.... cold....');
+        message.channel.sendMessage({files: ["https://cdn.discordapp.com/attachments/347254691966615552/748290292049772554/mario_wash.mp4"]});
     }
     if(message.content.includes("it's monday")) {
-        message.channel.sendMessage('https://styles.redditmedia.com/t5_24n3x8/styles/communityIcon_8lg3bhsw3ul31.jpg?format=pjpg&s=70bcc06a1e4c333d3a2e07df21cd417554d64c8b');
+        message.channel.sendMessage({files: ["https://cdn.discordapp.com/attachments/738573605268619295/841193029154111488/video0-8.mp4"]});
     }
     if(message.content.includes("it's Monday")) {
-        message.channel.sendMessage('https://styles.redditmedia.com/t5_24n3x8/styles/communityIcon_8lg3bhsw3ul31.jpg?format=pjpg&s=70bcc06a1e4c333d3a2e07df21cd417554d64c8b');
+        message.channel.sendMessage({files: ["https://cdn.discordapp.com/attachments/738573605268619295/841193029154111488/video0-8.mp4"]});
     }
     if(message.content.includes("It's monday")) {
-        message.channel.sendMessage('https://styles.redditmedia.com/t5_24n3x8/styles/communityIcon_8lg3bhsw3ul31.jpg?format=pjpg&s=70bcc06a1e4c333d3a2e07df21cd417554d64c8b');
+        message.channel.sendMessage({files: ["https://cdn.discordapp.com/attachments/738573605268619295/841193029154111488/video0-8.mp4"]});
     }
     if(message.content.includes("It's Monday")) {
-        message.channel.sendMessage('https://styles.redditmedia.com/t5_24n3x8/styles/communityIcon_8lg3bhsw3ul31.jpg?format=pjpg&s=70bcc06a1e4c333d3a2e07df21cd417554d64c8b');
+        message.channel.sendMessage({files: ["https://cdn.discordapp.com/attachments/738573605268619295/841193029154111488/video0-8.mp4"]});
     }
     
     if(message.content.includes("its monday")) {
-        message.channel.sendMessage('https://styles.redditmedia.com/t5_24n3x8/styles/communityIcon_8lg3bhsw3ul31.jpg?format=pjpg&s=70bcc06a1e4c333d3a2e07df21cd417554d64c8b');
+        message.channel.sendMessage({files: ["https://cdn.discordapp.com/attachments/738573605268619295/841193029154111488/video0-8.mp4"]});
     }
     if(message.content.includes("its Monday")) {
-        message.channel.sendMessage('https://styles.redditmedia.com/t5_24n3x8/styles/communityIcon_8lg3bhsw3ul31.jpg?format=pjpg&s=70bcc06a1e4c333d3a2e07df21cd417554d64c8b');
+        message.channel.sendMessage({files: ["https://cdn.discordapp.com/attachments/738573605268619295/841193029154111488/video0-8.mp4"]});
     }
     if(message.content.includes("Its monday")) {
-        message.channel.sendMessage('https://styles.redditmedia.com/t5_24n3x8/styles/communityIcon_8lg3bhsw3ul31.jpg?format=pjpg&s=70bcc06a1e4c333d3a2e07df21cd417554d64c8b');
+        message.channel.sendMessage({files: ["https://cdn.discordapp.com/attachments/738573605268619295/841193029154111488/video0-8.mp4"]});
     }
     if(message.content.includes("Its Monday")) {
-        message.channel.sendMessage('https://styles.redditmedia.com/t5_24n3x8/styles/communityIcon_8lg3bhsw3ul31.jpg?format=pjpg&s=70bcc06a1e4c333d3a2e07df21cd417554d64c8b');
+        message.channel.sendMessage({files: ["https://cdn.discordapp.com/attachments/738573605268619295/841193029154111488/video0-8.mp4"]});
     }
     if(message.content.includes("Ballin")) {
-        message.channel.sendMessage('v=irEb9TS9yEk');
+        message.channel.sendMessage('https://cdn.discordapp.com/attachments/618993874399920144/867220152990171156/mario.mp4');
     }
     if(message.content.includes("ballin")) {
-        message.channel.sendMessage('v=irEb9TS9yEk');
+        message.channel.sendMessage('https://cdn.discordapp.com/attachments/618993874399920144/867220152990171156/mario.mp4');
     }
     if(message.content.includes("All Aboard")) {
-        message.channel.sendMessage('v=Wmhfn3mgWUI');
+        message.channel.sendMessage('https://youtu.be/YZFrAdtqQk8');
     }
     if(message.content.includes("all aboard")) {
-        message.channel.sendMessage('v=Wmhfn3mgWUI');
+        message.channel.sendMessage('https://youtu.be/YZFrAdtqQk8');
     }
     if(message.content.includes("all Aboard")) {
-        message.channel.sendMessage('v=Wmhfn3mgWUI');
+        message.channel.sendMessage('https://youtu.be/YZFrAdtqQk8');
     }
     if(message.content.includes("All aboard")) {
-        message.channel.sendMessage('v=Wmhfn3mgWUI');
+        message.channel.sendMessage('https://youtu.be/YZFrAdtqQk8');
     }
     if(message.content.includes("milk")) {
-        message.channel.sendMessage('RmluZGluZyB0aGUgdHJ1dGg=');
+        message.channel.sendMessage('https://cdn.discordapp.com/attachments/594373333412347915/681051535039266846/cool_but_dairy_and_THICC.png');
     }
     if(message.content.includes("Milk")) {
-        message.channel.sendMessage('RmluZGluZyB0aGUgdHJ1dGg=');
+        message.channel.sendMessage('https://cdn.discordapp.com/attachments/594373333412347915/681051535039266846/cool_but_dairy_and_THICC.png');
     }
     if (message.content.includes("Noodle")) {
-        message.channel.sendMessage('https://vignette.wikia.nocookie.net/creepypasta/images/d/d2/Thisman.jpg/revision/latest?cb=20131118111953');
+        message.channel.sendMessage('https://cdn.discordapp.com/attachments/594373333412347915/623775240375500800/just_buy_a_new_one.mp4');
     }
     if (message.content.includes("noodle")) {
-        message.channel.sendMessage('https://vignette.wikia.nocookie.net/creepypasta/images/d/d2/Thisman.jpg/revision/latest?cb=20131118111953');
+        message.channel.sendMessage('https://cdn.discordapp.com/attachments/594373333412347915/623775240375500800/just_buy_a_new_one.mp4');
     }
-    if (message.content.includes("spook")) {
+    if(message.content.includes("spook")) {
         message.channel.sendMessage('https://youtu.be/16-aGc2Win0');
     }
-    if (message.content.includes("Spook")) {
+    if(message.content.includes("Spook")) {
         message.channel.sendMessage('https://youtu.be/16-aGc2Win0');
-    }
-    if (message.content.includes("!plshelp")) {
-        message.channel.sendMessage({files: ["https://raw.githubusercontent.com/KimsoftStudio/MemeBot/master/commands.png"]});
     }
 });
 
